c957e7b2a163bf39f25b5efa4dd1a6a0c2bbe4d3
Duouk2000 <duouk2000@gmail.com>
2024-02-04 00:11:34 +0000

    added js code to allow you to search for a drink as well as generating a random drink on refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,106 @@
-// use Jung's code to get around the CORS issue
-const url = "https://cors-anywhere-jung-48d4feb9d097.herokuapp.com/" + 'www.thecocktaildb.com/api/json/v1/1/random.php'
+// use Jung's code to get around the CORS issue and use the random api search
+const randomurl = 'https://cors-anywhere-jung-48d4feb9d097.herokuapp.com/' + 'www.thecocktaildb.com/api/json/v1/1/random.php'
+
+ fetch(randomurl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (drinksData) {
+      // console log data to work out the data structure
+      console.log(drinksData);
+      // console log various bits of data from the JSON
+      console.log('Drink Name:', drinksData.drinks[0].strDrink);
+      console.log('Drink Type:', drinksData.drinks[0].strAlcoholic);
+      console.log('Category:', drinksData.drinks[0].strCategory);
+      console.log('Recipe:', drinksData.drinks[0].strInstructions);
+      console.log('Thumbnail:', drinksData.drinks[0].strDrinkThumb);
+
+      // update HTML elements with drink information
+      document.getElementById('drinkName').innerText = `Drink Name: ${drinksData.drinks[0].strDrink}`;
+      document.getElementById('drinkType').innerText = `Drink Type: ${drinksData.drinks[0].strAlcoholic}`;
+      document.getElementById('category').innerText = `Category: ${drinksData.drinks[0].strCategory}`;
+      document.getElementById('recipe').innerText = `Recipe: ${drinksData.drinks[0].strInstructions}`;
+      // update the thumbnail image in the HTML element
+      const thumbNailElement = document.getElementById('thumbnail');
+      thumbNailElement.src = drinksData.drinks[0].strDrinkThumb;
+
+      // ingredient lists vary in size per drink. the below code loops through the array to extract each one
+
+      // HTML element where ingredient information will be displayed
+      const drinksSection = document.getElementById('ingredient');
+      // loop through the array to extract and display ingredients
+      for (let i = 0; i <= 14; i++) {
+        const ingredientKey = `strIngredient${i}`;
+        const ingredient = drinksData.drinks[0][ingredientKey];
+        // check if ingredients are listed, ignore if null
+        if (ingredient) {
+          // console log ingredients
+          console.log(`Ingredients ${i}: ${ingredient}`);
+          // create a new div for each ingredient found
+          const ingredientContainer = document.createElement('div');
+          ingredientContainer.innerText = `Ingredients ${i}: ${ingredient}`;
+          // append it to the ingredient id in the HTML element
+          drinksSection.appendChild(ingredientContainer);
+        }
+      }
+    });
+
+// listen for any input that is entered into the search box
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('search-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const drinkInput = document.getElementById('author-search').value;
+    getDrinkData(drinkInput);
+  });
+});
+ 
+// function to display the drink the user searches for
+function getDrinkData(drinkInput) {
+
+  // use Jung's code to get around the CORS issue and use the drink name search api
+const url = 'https://cors-anywhere-jung-48d4feb9d097.herokuapp.com/' + 'www.thecocktaildb.com/api/json/v1/1/search.php?s=' + drinkInput 
 
 fetch(url)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (drinksData) {
-    
-    // console log data to work out the data structure
-    console.log(drinksData);
-    // console log various bits of data from the JSON
-    console.log('Drink Name:', drinksData.drinks[0].strDrink);
-    console.log('Drink Type:', drinksData.drinks[0].strAlcoholic);
-    console.log('Category:', drinksData.drinks[0].strCategory);    
-    console.log('Recipe:', drinksData.drinks[0].strInstructions);
-    console.log('Thumbnail:', drinksData.drinks[0].strDrinkThumb);
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (drinksData) {
+      // console log data to work out the data structure
+      console.log(drinksData);
+      // console log various bits of data from the JSON
+      console.log('Drink Name:', drinksData.drinks[0].strDrink);
+      console.log('Drink Type:', drinksData.drinks[0].strAlcoholic);
+      console.log('Category:', drinksData.drinks[0].strCategory);
+      console.log('Recipe:', drinksData.drinks[0].strInstructions);
+      console.log('Thumbnail:', drinksData.drinks[0].strDrinkThumb);
 
-    // update HTML elements with drink information
-    document.getElementById('drinkName').innerText = `Drink Name: ${drinksData.drinks[0].strDrink}`
-    document.getElementById('drinkType').innerText = `Drink Type: ${drinksData.drinks[0].strAlcoholic}`
-    document.getElementById('category').innerText = `Category: ${drinksData.drinks[0].strCategory}`   
-    document.getElementById('recipe').innerText = `Recipe: ${drinksData.drinks[0].strInstructions}`
-    // update the thumbnail image in the HTML element
-    const thumbNailElement = document.getElementById('thumbnail');
-    thumbNailElement.src = drinksData.drinks[0].strDrinkThumb;
+      // update HTML elements with drink information
+      document.getElementById('drinkName').innerText = `Drink Name: ${drinksData.drinks[0].strDrink}`;
+      document.getElementById('drinkType').innerText = `Drink Type: ${drinksData.drinks[0].strAlcoholic}`;
+      document.getElementById('category').innerText = `Category: ${drinksData.drinks[0].strCategory}`;
+      document.getElementById('recipe').innerText = `Recipe: ${drinksData.drinks[0].strInstructions}`;
+      // update the thumbnail image in the HTML element
+      const thumbNailElement = document.getElementById('thumbnail');
+      thumbNailElement.src = drinksData.drinks[0].strDrinkThumb;
 
-    
-    // ingredient lists vary in size per drink. the below code loops through the array to extract each one
+      // ingredient lists vary in size per drink. the below code loops through the array to extract each one
 
-    // HTML element where ingredient information will be displayed
-    const drinksSection = document.getElementById('ingredient')
-    // loop through the array to extract and display ingredients
-    for (let i = 0; i <= 14; i++) {
-        const ingredientKey = `strIngredient${i}`;    
-        const ingredient = drinksData.drinks[0][ingredientKey];        
+      // HTML element where ingredient information will be displayed
+      const drinksSection = document.getElementById('ingredient');
+      // loop through the array to extract and display ingredients
+      for (let i = 0; i <= 14; i++) {
+        const ingredientKey = `strIngredient${i}`;
+        const ingredient = drinksData.drinks[0][ingredientKey];
         // check if ingredients are listed, ignore if null
-        if (ingredient ) {
-            // console log ingredients
-            console.log(`Ingredients ${i}: ${ingredient}`);
-            // create a new div for each ingredient found
-            const ingredientContainer = document.createElement('div');            
-            ingredientContainer.innerText = `Ingredients ${i}: ${ingredient}`;
-            // append it to the ingredient id in the HTML element 
-            drinksSection.appendChild(ingredientContainer)
-        }
-    }
-  })
-
-
-
- 
+        if (ingredient) {
+          // console log ingredients
+          console.log(`Ingredients ${i}: ${ingredient}`);
+          // create a new div for each ingredient found
+          const ingredientContainer = document.createElement('div');
+          ingredientContainer.innerText = `Ingredients ${i}: ${ingredient}`;
+          // append it to the ingredient id in the HTML element
+          drinksSection.appendChild(ingredientContainer);
+        };
+      };
+    });
+  };
